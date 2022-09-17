432a98878f5dd3c5cc69cae4429a9e7ad8374393
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-09-16 21:19:58 -0500

    Update RockPaperScissors.py

--- games/RockPaperScissors.py
+++ games/RockPaperScissors.py
@@ -7,19 +7,26 @@ A rock paper scissors game.
 
 @author: LZ-FSDev
 @Python version: Python 3.9
-@version: 0.0.1
+@version: 0.0.2
 """
 
 print("Welcome to rock paper scissors!")
 print()
 
+userChoice = ""
+choices = ["Rock", "Paper", "Scissors"]
+
 userChoice = input("Please type in console what you would like to go: ")
 print()
 
+while (userChoice not in choices):
+    print()
+    print("That is not a valid option!")
+    userChoice = input("Please type in console what you would like to go: ")
+
+print()
 print("Rock, Paper, Scissors!")
 print()
-
-choices = ["Rock", "Paper", "Scissors"]
 
 #generating random int between 0 and 2 inclusive
 randomChoice = random.randint(0,2)
