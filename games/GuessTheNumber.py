9183b11fdbadf723dff7ed7877009c226faf70f3
yurkuul <yurkuull@gmail.com>
2022-09-23 19:07:25 -0500

    modified

--- games/GuessTheNumber.py
+++ games/GuessTheNumber.py
@@ -5,7 +5,7 @@ Guess the number game.
 
 @author: LZ-FSDev
 @Python version: Python 3.9
-@version: 0.0.1
+@version: 0.0.2
 """
 
 print("Welcome to Guess The Number!")
@@ -22,5 +22,19 @@ while (highRange < lowRange):
 print()
 
 number = random.randint(lowRange, highRange)
+stillGuessing = True
 
 print("A random number has been created within the range of {} and {}. Start guessing!".format(lowRange, highRange))
+print()
+
+guess = int(input("Enter your guess: "))
+
+while (stillGuessing):
+    if (guess == number):
+        print("You win!")
+        break
+    if (guess < lowRange or guess > highRange):
+        print("That's outside the guessing range!")
+        print()
+    guess = int(input("Enter your guess: "))
+    print()
