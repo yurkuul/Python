3c7bfedb91d185117e8fc91ac630f20fae409329
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-09-27 13:10:53 -0500

    Update GuessTheNumber.py

--- games/GuessTheNumber.py
+++ games/GuessTheNumber.py
@@ -5,7 +5,7 @@ Guess the number game.
 
 @author: LZ-FSDev
 @Python version: Python 3.9
-@version: 0.0.4
+@version: 0.0.5
 """
 
 print("Welcome to Guess The Number!")
@@ -37,26 +37,41 @@ while (choosingNumber):
 highRange = int(highRange)
 
 print()
+print("Setting up your game...")
+
+difference = highRange - lowRange
+if (difference > 5):
+    guesses = difference // 5
+else:
+    guesses = 1
 
 number = random.randint(lowRange, highRange)
 stillGuessing = True
 
-print("A random number has been created within the range of {} and {}. Start guessing!".format(lowRange, highRange))
+print("A random number has been created within the range of {} and {}. You have {} guess(es). Goodluck!".format(lowRange, highRange, guesses))
 print()
 
 guess = int(input("Enter your guess: "))
 
-while (stillGuessing):
+while (guesses > 0 and stillGuessing):
     difference = guess - number
-    if (guess < lowRange or guess > highRange):
-        print("That's outside the guessing range!")
-    elif (difference <= 10 and difference > 0):
-        print("You are within 10 digits above the number!")
-    elif (abs(difference) <= 10):
-        print("You are within 10 digits below the number!")
-    print()
-    guess = int(input("Enter your guess: "))
-    print()
     if (guess == number):
         print("You win!")
         stillGuessing = False
+    elif (guess < lowRange or guess > highRange):
+        print("That's outside the guessing range!")
+    elif (difference <= 10 and difference > 0):
+        guesses -= 1
+        print("You are within 10 digits above the number!")
+    elif (abs(difference) <= 10):
+        guesses -= 1
+        print("You are within 10 digits below the number!")
+    print()
+    if (guesses == 0):
+        print("You were unable to guess the number :( Better luck next time!")
+        print("The number was " + str(number) + ".")
+        stillGuessing = False
+    elif (stillGuessing):
+        print("You have {} guesses left.".format(guesses))
+        guess = int(input("Enter your guess: "))
+        print()
