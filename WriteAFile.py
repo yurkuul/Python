cdc53bcaef3f76bcc81b24b97e71bf1486071806
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-10-13 18:47:58 -0500

    Create WriteAFile.py

---
+++ WriteAFile.py
@@ -0,0 +1,46 @@
+"""
+Grabs user input and appends to a file. Once the file closes, the number of
+lines, words, and characters from the file will be printed.
+
+@author: Lisa
+@Python version: Python 3.8.10
+@version: 0.0.1
+"""
+
+userFile = open("user_file.txt", "w")
+lines = 1
+words = 0
+characters = 0
+totalLines = 0
+totalWords = 0
+totalCharacters = 0
+
+print("Welcome to Write-A-File!")
+print("------------------------")
+
+print("This is a demo for writing to a text file and to print out the " \
+      "analytics of the file created.")
+    
+print("----------------------------------------------------------------------")
+
+print("As the user, you will be creating the file via input you provide.")
+
+print("We will provide the number of lines, words, and characters based " \
+      "on your input.")
+
+theLine = input("You may begin adding to the file. Type 'q' to quit: ")
+    
+while (theLine != 'q'):
+    userFile.write(theLine + "\n")
+    theLine = input("new line: ")
+
+userFile.close()
+
+print("-----------------------------------------------")
+
+userFile = open("user_file.txt")
+
+for line in userFile:
+    print("Line " + str(lines))
+    print (line)
+    lines+=1
