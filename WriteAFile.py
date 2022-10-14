0d153947c8e2d33e76a0199fd3501f2f3318f84d
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-10-14 17:45:56 -0500

    Update WriteAFile.py

--- WriteAFile.py
+++ WriteAFile.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 """
 Grabs user input and appends to a file. Once the file closes, the number of
 lines, words, and characters from the file will be printed.
 
 @author: Lisa
 @Python version: Python 3.8.10
-@version: 0.0.1
+@version: 0.0.2
 """
 
 userFile = open("user_file.txt", "w")
@@ -44,3 +45,14 @@ for line in userFile:
     print("Line " + str(lines))
     print (line)
     lines+=1
+    totalLines+=1
+    totalWords += len(line.strip().split())
+    for character in line:
+        totalCharacters +=1
+
+print("----------------------------------------------------------------------")
+
+print("The file that you just created has {} lines.".format(totalLines))
+print("From those lines, you have {} words.".format(totalWords))
+print("Your file contains {} characters, including white spaces."
+      .format(totalCharacters))
