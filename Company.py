d2d05b626bb39bc891cc10c9a0cb760bcf668fde
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-11-26 00:49:04 -0600

    Update Company.py

--- Company.py
+++ Company.py
@@ -31,3 +31,34 @@ class Employee():
         
     def setSalary(self, sal):
         self.salary = sal
+
+class Project():
+    title = ""
+    budget = 0.0
+    dueDate = ""
+    employees = list()
+    def __init__(self, theTitle, money, deadline):
+        self.title = theTitle
+        self.budget = money
+        self.dueDate = deadline
+    
+    def addTeamMember(self, anEmployee):
+        self.employees.append(anEmployee)
+    
+    def __str__(self):
+        return "Project title: {}, with a budget of ${:.2f}, due {}. Employees working on this project: {}".format(self.title, self.budget, self.dueDate, self.employees)
+        
+    def getTitle(self):
+        return self.title
+    
+    def getBudget(self):
+        return self.budget
+    
+    def getDueDate(self):
+        return self.dueDate
+    
+    def setBudget(self, money):
+        self.budget = money
+        
+    def setDueDate(self, deadline):
+        self.dueDate = deadline    
