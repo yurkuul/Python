6a4ef46490fb53abf2ea55c72ad7e554bd0928b9
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-11-27 12:13:18 -0600

    Update Company.py

--- Company.py
+++ Company.py
@@ -62,3 +62,28 @@ class Project():
         
     def setDueDate(self, deadline):
         self.dueDate = deadline    
+
+       
+class Company():
+    employees = list()
+    projects = list()
+    
+    def addStaff(self, name, idNum, salary):
+        employee = Employee(name, idNum, salary)
+        self.employees.append(employee)
+    
+    def addProject(self, title, budget, dueDate):
+        project = Project(title, budget, dueDate)
+        self.projects.append(project)
+    
+    def assignEmpToProj(self, employeeName, projTitle):
+        theEmployee = None
+        theProject = None
+        for person in self.employees:
+            if (person.getName() == employeeName):
+                theEmployee = person
+        for project in self.projects:
+            if (project.getTitle() == projTitle):
+                theProject = project
+        theEmployee.addProject(theProject)
+        theProject.addTeamMember(theEmployee)
