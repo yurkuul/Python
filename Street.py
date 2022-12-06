0fe9a0d42c2b8e00a845d23e636920c97257dbbd
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-12-06 10:01:21 -0600

    Update Street.py

--- Street.py
+++ Street.py
@@ -28,3 +28,24 @@ class Street():
         Returns the condition of the street.
         """
         return self.condition
+
+    def compare(self, otherStreet):
+        """
+        Compares the street that calls this method with a second street.
+
+        Return True if the first street is in need of urgent repair compared to
+        the second street.
+        """
+        firstRepair = True
+        if (self.condition == "good" and otherStreet.getCondition() != "good"):
+            firstRepair = False
+        elif (self.condition == "fair" and otherStreet.getCondition() == "poor"):
+            firstRepair = False
+        elif (self.condition == otherStreet.getCondition()):
+            if (self.getCarsTravelled() < otherStreet.getCarsTravelled()):
+                firstRepair = False
+        return firstRepair
+    
+    def __str__(self):
+        return "{} is {} km long, sees {} cars per day and is in {} condition.".format(
+            self.name, self.length, self.carsTravelled, self.condition)
